f9bc48383df0ddb4bf8eeb0e05ccc4de121ebff0
Amos Mwansa <mwansa.amosb@gmail.com>
2026-03-11 10:40:14 +0200

    commit2

--- src/App.js
+++ src/App.js
@@ -27,9 +27,9 @@ import {
 import eventPoster from "./assets/event-poster.jpeg";
 import slide1 from "./assets/slide1.jpg";
 import slide2 from "./assets/slide2.jpg";
-import slide3 from "./assets/slide3.jpg";
+import slide3 from "./assets/slide3.jpeg";
 import slide4 from "./assets/slide4.jpg";
-import slide5 from "./assets/slide5.jpg";
+import slide5 from "./assets/slide5.jpeg";
 import slide6 from "./assets/slide6.jpg";
 
 import sponsor1 from "./assets/sponsor logos/coolbrain.png";
